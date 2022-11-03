0cbeabfd0c63dc3f8a8f18154f9c3943fca68d26
Bnaya <bnaya98@gmail.com>
2022-11-03 18:45:19 +0200

    sign up bug fixes

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { useNavigate, useOutletContext } from 'react-router-dom';
 
 export const SignupPage = () => {
 
@@ -8,30 +8,36 @@ export const SignupPage = () => {
   const phoneRef = useRef()
   const nameRef = useRef()
   const statusRef = useRef()
+  const navigate = useNavigate()
+
   const getFormInputs = () => ({
     phone: phoneRef.current.value,
     name: nameRef.current.value,
     status: statusRef.current.value,
   })
 
+  const onCloseModal = () => {
+    navigate('/')
+  }
+
   return (
-    <section className='screen'>
-    <form className='signup-page' onSubmit={(ev) => onSignup(ev,getFormInputs())}>
-      <h1 className='signup-title'>Sign Up to WusApp</h1>
-      <section className='form-input'>
-        <label htmlFor="phone">Phone</label>
-        <input ref={phoneRef} name='phone' type='text' />
-      </section>
-      <section className='form-input'>
-        <label htmlFor="name">Name</label>
-        <input ref={nameRef} name='name' type="text" />
-      </section>
-      <section className='form-input'>
-        <label htmlFor="status">Status</label>
-        <input ref={statusRef} name='status' type='text' />
-      </section>
-      <button className='btn'>Sign Up</button>
-    </form>
+    <section className='screen' onClick={onCloseModal}>
+      <form className='signup-page' onSubmit={(ev) => onSignup(ev, getFormInputs())} onClick={(e) => e.stopPropagation()}>
+        <h1 className='signup-title'>Sign Up to WusApp</h1>
+        <section className='form-input'>
+          <label htmlFor="phone">Phone</label>
+          <input ref={phoneRef} name='phone' type='text' />
+        </section>
+        <section className='form-input'>
+          <label htmlFor="name">Name</label>
+          <input ref={nameRef} name='name' type="text" />
+        </section>
+        <section className='form-input'>
+          <label htmlFor="status">Status</label>
+          <input ref={statusRef} name='status' type='text' />
+        </section>
+        <button className='btn'>Sign Up</button>
+      </form>
     </section>
   )
 }
